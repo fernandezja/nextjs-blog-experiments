8a5858fc4d764771a4b05373382668b1f0367bc4
fernandezja <fernandezja@gmail.com>
2020-09-14 23:45:41 -0300

    Dynamic Routes: Render Markdown

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -4,14 +4,16 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
     return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    )
+        <Layout>
+          {postData.title}
+          <br />
+          {postData.id}
+          <br />
+          {postData.date}
+          <br />
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+      )
   }
 
 export async function getStaticPaths() {
@@ -24,7 +26,7 @@ export async function getStaticPaths() {
 
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
       props: {
         postData
